c3a0c7a13cec4d053c2d484d839b4c0943ca7c82
aaronk2002 <aaronakj2002@gmail.com>
2024-09-28 14:51:27 -0400

    Removed supercloud loading

--- real-data/bash_scripts/imdb_2_model_4.sh
+++ real-data/bash_scripts/imdb_2_model_4.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# Loading Modules in Supercloud
-source /etc/profile
-module load anaconda/2023a-pytorch
-module load cuda/11.8
-
 python3 ../train.py --config ../configs/2/model_size_4.json
