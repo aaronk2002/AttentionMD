2e3222d27c51439e7ed11af264faf77402d6107a
aaronk2002 <65597169+aaronk2002@users.noreply.github.com>
2024-09-30 23:22:17 -0400

    Update run-joint-3.sh

--- synthetic-data/bash_scripts/run-joint-3.sh
+++ synthetic-data/bash_scripts/run-joint-3.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# Loading Modules in Supercloud
-source /etc/profile
-module load anaconda/2023b
-module load cuda/11.8
-
 # Single Result
 python ../joint_training.py -p 3 --lr 0.1 --epochs 20_000 --output ../result/joint_convergence/3.pt
